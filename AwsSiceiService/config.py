a1a7174e24232411e9ee5e0c91eb14166fcf0c8a
Samuel Rodriguez <116043050+Samux550@users.noreply.github.com>
2025-12-10 15:54:40 -0600

    Remove default DATABASE_URL value in config
    Removed default value for DATABASE_URL in config.

--- AwsSiceiService/config.py
+++ AwsSiceiService/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///dev.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     AWS_REGION = os.environ.get("AWS_REGION", "us-east-1")
@@ -11,3 +11,4 @@ class Config:
     S3_BUCKET = os.environ.get("S3_BUCKET")
     SNS_TOPIC_ARN = os.environ.get("SNS_TOPIC_ARN")
     DYNAMO_TABLE = os.environ.get("DYNAMO_TABLE", "sesiones-alumnos")
+
